1ebae4b39f729b4d84af85cfc657047ae289242a
adrianit248 <adrianit248@gmail.com>
2025-06-23 23:13:37 -0400

    switched to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
 import Home from './pages/Home'
 import Movies from './pages/Movies'
 import { useState } from "react";
@@ -8,14 +8,14 @@ function App(props) {
   const [idVal, setIdVal] = useState('')
 
   return (
-  <Router>
+  <HashRouter>
     <div className="App">
       <Routes>
         <Route path='/' element={<Home idVal={idVal} setIdVal={setIdVal} />}></Route>
         <Route path=':id' element={<Movies idVal={idVal} setIdVal={setIdVal} />}></Route>
       </Routes>
     </div>
-  </Router>
+  </HashRouter>
   );
 }
 
